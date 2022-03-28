f1d0e2ac38aa22711ef311d55d5e5297324b72e6
Meena Garai <68517659+MEENAGARAI@users.noreply.github.com>
2022-03-28 16:12:53 +0530

    Add files via upload

---
+++ FPS-3D-Game-main/shoot.js
@@ -0,0 +1,73 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var bullet = document.createElement("a-entity");
+
+        bullet.setAttribute("geometry", {
+          primitive: "sphere",
+          radius: 0.1,
+        });
+
+        bullet.setAttribute("material", "color", "black");
+        bullet.setAttribute("dynamic-body", {shape: "sphere", mass: "0"})
+
+        var cam = document.querySelector("#camera-rig");
+
+        pos = cam.getAttribute("position");
+
+        bullet.setAttribute("position", {
+          x: pos.x,
+          y: pos.y + 1,
+          z: pos.z - 0.5,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+
+        //set the velocity and it's direction
+        bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+
+        var scene = document.querySelector("#scene");
+
+        bullet.setAttribute("dynamic-body", {shape: "sphere", mass: "50"})     
+
+        bullet.addEventListener("collide", 
+          this.removeBullet
+        )
+
+        scene.appendChild(bullet);
+      }
+    });
+  },
+
+  removeBullet: function(e)
+  {
+    var scene = document.querySelector("#scene");
+    var element = e.detail.target.el;
+    var elementhit = e.detail.body.el;
+
+    if(elementhit.id.includes("enemy"))
+    {
+      var counttankEl = document.querySelector("#countTank");
+      var tanksfired = parseInt(counttankEl.getAttribute("text").value);
+      tanksfired -= 1
+      counttankEl.setAttribute("text", {value: tanksfired})
+
+      if(tanksfired === 0){
+        var txt = document.querySelector("#completed");
+        txt.setAttribute("visible", true)
+      }
+
+      scene.removeChild(elementhit)
+    }
+    element.removeEventListener("collide", this.removeBullet);
+    scene.removeChild(element)
+  }
+});
